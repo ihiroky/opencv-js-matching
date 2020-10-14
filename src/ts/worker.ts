6efce8f61353a8203ed28d3ff5e446925300aa6b
Hiroki Itoh <hiroki.itoh@techtouch.co.jp>
2020-10-14 15:01:52 +0900

    Find distant points to calculate target region

--- src/ts/worker.ts
+++ src/ts/worker.ts
@@ -17,6 +17,73 @@ cv().then(cv => {
   console.log('worker is initialized.')
 })
 
+function searchFarthestPoints(
+  target: ImageData,
+  whole: ImageData,
+  kp1: KeyPointVector,
+  kp2: KeyPointVector,
+  good: DMatchVectorVector
+): { p1: Point, p2: Point }[] {
+  if (good.size() <= 1) {
+    throw new Error('Not enough good points.')
+  }
+
+  const width = Math.max(target.width, whole.height)
+  const height = Math.max(target.height, whole.height)
+  const tlbr: { p: Point, qi: number, ti: number }[] = [
+    { p: { x: width, y: height }, qi: 0, ti: 0 }, // top
+    { p: { x: width, y: height }, qi: 0, ti: 0 }, // left
+    { p: { x: 0, y: 0 }, qi: 0, ti: 0 }, // bottom
+    { p: { x: 0, y: 0 }, qi: 0, ti: 0 }, // right
+  ]
+  for (let i = 0; i < good.size(); i++) {
+    const m = good.get(i).get(0)
+    const m2 = kp2.get(m.trainIdx)
+    let updateTlbrIndex: number = -1
+    if (m2.pt.y < tlbr[0].p.y) {
+      updateTlbrIndex = 0
+    }
+    if (m2.pt.x < tlbr[1].p.x) {
+      updateTlbrIndex = 1
+    }
+    if (m2.pt.y > tlbr[2].p.y) {
+      updateTlbrIndex = 2
+    }
+    if (m2.pt.x > tlbr[3].p.x) {
+      updateTlbrIndex = 3
+    }
+    if (updateTlbrIndex >= 0) {
+      const t = tlbr[updateTlbrIndex]
+      t.p = m2.pt
+      t.qi = m.queryIdx
+      t.ti = m.trainIdx
+    }
+  }
+  let maxSquaredDistance = 0
+  let maxSdIndexPair: { i1: number, i2: number } = { i1: 0, i2: 0 }
+  for (let i = 0; i < tlbr.length; i++) {
+    for (let ii = i + 1; ii < tlbr.length; ii++) {
+      const p1 = tlbr[i].p
+      const p2 = tlbr[ii].p
+      const sd = (p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2
+      if (sd > maxSquaredDistance) {
+        maxSquaredDistance = sd
+        maxSdIndexPair = { i1: i, i2: ii }
+      }
+    }
+  }
+  return [
+    {
+      p1: kp1.get(tlbr[maxSdIndexPair.i1].qi).pt,
+      p2: kp1.get(tlbr[maxSdIndexPair.i2].qi).pt
+    },
+    {
+      p1: kp2.get(tlbr[maxSdIndexPair.i1].ti).pt,
+      p2: kp2.get(tlbr[maxSdIndexPair.i2].ti).pt
+    },
+  ]
+}
+
 function match(cv: OpenCVObject, target: ImageData, whole: ImageData): void {
   const rawTarget = cv.matFromImageData(target)
   const targetImage = new cv.Mat()
@@ -49,8 +116,13 @@ function match(cv: OpenCVObject, target: ImageData, whole: ImageData): void {
       good.push_back(t)
     }
   }
+
   const matchingImage = new cv.Mat()
   cv.drawMatchesKnn(targetImage, kp1, wholeImage, kp2, good, matchingImage)
+
+  const ps = searchFarthestPoints(target, whole, kp1, kp2, good)
+  console.log('The farthest points of target', ps[0])
+  console.log('The farthest points of whole', ps[1])
 
   const result = new cv.Mat()
   cv.cvtColor(matchingImage, result, cv.COLOR_RGB2RGBA, 0)
